e0b0a450eaf3ed114d3c497590d0aff486e27914
xiong19971207 <278720216@qq.com>
2022-03-19 17:44:39 +0800

    delete __pycache__

--- day01/day010/settings.py
+++ day01/day010/settings.py
@@ -81,12 +81,10 @@ WSGI_APPLICATION = 'day010.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE':'django.db.backends.mysql',
-        'NAME': 'day010',
-        'USER':'xiong',
-        'PASSWORD':'123',
+        'NAME': 'axf',
+        'USER':'root',
+        'PASSWORD':'1234',
         'HOST':'localhost',
         'PORT':'3306',
     }
